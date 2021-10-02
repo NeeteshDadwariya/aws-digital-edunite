8048450a5b9eb1aa89e5e8ab000198dd26d6bad9
kkbacktracks <kaustubhrk10@gmail.com>
2021-10-01 22:18:41 -0500

    Initial Commit

---
+++ smsbackend/src/main/java/com/hackdfw/smsbackend/EduniteApplication.java
@@ -0,0 +1,13 @@
+package com.hackdfw.smsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduniteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduniteApplication.class, args);
+	}
+
+}
---
+++ smsbackend/src/test/java/com/hackdfw/smsbackend/EduniteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackdfw.smsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduniteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
